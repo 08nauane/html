cc171a9390410ae964f69e98d5c0ad187e8dfe62
08nauane <nauane.francisco@escola.pr.gov.br>
2025-10-08 09:48:27 -0300

    Add files via upload

--- lista de compra/carregar.js
+++ lista de compra/carregar.js
@@ -17,3 +17,29 @@ inputArquivo.style.display = 'none';
 document.body.appendChild(inputArquivo);
 
 // Função para processar o arquivo selecionado
+function carregarListaDoArquivo(file) {
+    const reader = new FileReader();
+    reader.onload = function () {
+        // Limpa a lista atual (opcional)
+        lista.innerHTML = '';
+
+        // Divide o conteudo em linhas e adiciona cada uma como <li>
+        const linhas = reader.result.split(/\r?\n/).filter (l => l.trim()!=='');
+        linhas.forEach(linha => {
+          const li = document.createElement('li')
+          li.textContent = linha;
+          li.style.padding = '4px 0';
+
+          li.style.borderBottom = '1px solid ###ccc'
+          // linha fina cinza
+
+          linha.appendChild(li)
+
+        } );
+
+    };
+    reader.dAsText(file);
+}
+//eventos
+btnCarregar.addEventListener('click', () =>
+)
